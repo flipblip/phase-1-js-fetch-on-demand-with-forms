da00dd7c2515ec591d3d8f34679597581f85fded
fliblip <wickynjy@gmail.com>
2022-09-07 00:11:28 +0300

    Done with assigment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+
+
+        // console.log(event.target.children)
+        // console.log(event.target.children[1].value)
+        // console.log(input.value)
+
+        // fetch(`http://localhost:3000/movies/${input.value}`)
+        // .then(response => response.json())
+        // .then(data => {
+        //     console.log(data);
+        // })
+
+        //modify DOM with data from server
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p')
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+    });
   
 }
 
